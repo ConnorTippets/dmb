244161a83c1e581e7989158c2ef2119396315a55
ConnorTippets <meiscool466@gmail.com>
2025-12-23 20:39:56 -0600

    refactor: move command into root category

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from discord.ext.commands import (
     CommandError,
     CommandNotFound,
     CheckFailure,
+    command,
+    is_owner,
+    ExtensionFailed,
 )
 from utilities.config import config, config_to_options
 from utilities.cogs import load_cogs
+import traceback
 
 bot = Bot(**config_to_options())
 load_cogs(bot)
@@ -30,6 +34,19 @@ async def on_command_error(ctx: Context, exception: CommandError):
 
         # for logging the error
         await Bot.on_command_error(bot, ctx, exception)
+
+
+@command(brief="Reloads a cog.")
+@is_owner()
+async def reload_extension(ctx: Context, *, extension: str):
+    try:
+        if not extension == "jishaku":
+            extension = f"commands.{extension}"
+        await bot.reload_extension(extension)
+        await ctx.message.add_reaction("\U00002705")
+    except ExtensionFailed as e:
+        tb = "".join(traceback.format_exception(type(e), e, e.__traceback__))
+        await ctx.send(f"```py\n{tb}```")
 
 
 bot.run(config.token)
